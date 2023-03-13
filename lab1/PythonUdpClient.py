06bce84ed6901cfc54a031e962408ac409b2c459
Przemek Weglik <przemek.weglik@onet.pl>
2023-03-13 15:11:30 +0100

    Reformat Udp client and server from lab1

--- lab1/PythonUdpClient.py
+++ lab1/PythonUdpClient.py
@@ -1,18 +1,18 @@
-import socket;
+import socket
 
 serverIP = "127.0.0.1"
 serverPort = 9008
 buff = []
 
-print('PYTHON UDP CLIENT')
+print("PYTHON UDP CLIENT")
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.connect((serverIP, serverPort))
 
-msg_bytes = (2).to_bytes(4, byteorder='little')
-print('Send: '  + str(int.from_bytes(msg_bytes, byteorder='little'))) 
+msg_bytes = (2).to_bytes(4, byteorder="little")
+print("Send: " + str(int.from_bytes(msg_bytes, byteorder="little")))
 client.send(msg_bytes)
 
 
 data = client.recv(1024)
 
-print('Received from server: ' + str(int.from_bytes(data, byteorder='little'))) 
+print("Received from server: " + str(int.from_bytes(data, byteorder="little")))
--- lab1/PythonUdpServer.py
+++ lab1/PythonUdpServer.py
@@ -1,16 +1,13 @@
-import socket;
+import socket
 
 serverPort = 9009
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+serverSocket.bind(("", serverPort))
 buff = []
 
-print('PYTHON UDP SERVER')
+print("PYTHON UDP SERVER")
 
 while True:
 
     buff, address = serverSocket.recvfrom(1024)
-    print("python udp server received msg: " + str(buff, 'cp1250'))
-
-
-
+    print("python udp server received msg: " + str(buff, "cp1250"))
